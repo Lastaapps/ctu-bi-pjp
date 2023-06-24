0b33b051f03997200507ba08743c24aadbbdf820
Petr Laštovička <lastope2@fit.cvut.cz>
2023-06-24 17:43:25 +0200

    build: Add Rust to the mila script

---
+++ src/lib.c
@@ -0,0 +1,14 @@
+#include<stdint.h>
+#include<stdio.h>
+
+int64_t built_read_line(int64_t* ptr) {
+    return scanf("%lld", ptr);
+}
+
+int64_t built_write(char* ptr) {
+    return printf("%s", ptr);
+}
+
+int64_t built_print(int64_t* val) {
+    return printf("%lld", val);
+}
